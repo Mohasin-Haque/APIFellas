2ca72c466024191081cef5c45fcd75fbf07f4469
Mohasin-Haque <mohasinhaque23121@gmail.com>
2021-10-15 11:07:30 +0530

    adding code in index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,22 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
+import './index.css'
+
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
+
+import * as React from 'react'
+import ReactDOM from 'react-dom'
+
+import HomeScene from './scenes/home'
+
+function App() {
+  return <HomeScene />
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
+
 export default function Home() {
   return (
     <div className={styles.container}>
